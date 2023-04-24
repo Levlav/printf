056750b6617ec66b6082df722024663ec522764d
charteym <charteym@gmail.com>
2023-04-24 12:53:50 -0700

    C - printf

--- get_precision.c
+++ get_precision.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * get_precision - Calculates the precision for printing
- * charteym
+ * By charteym and levin
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
@@ -15,9 +15,7 @@ int get_precision(const char *format, int *i, va_list list)
 
 	if (format[curr_i] != '.')
 		return (precision);
-
 	precision = 0;
-
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -34,8 +32,6 @@ int get_precision(const char *format, int *i, va_list list)
 		else
 			break;
 	}
-
 	*i = curr_i - 1;
-
 	return (precision);
 }
