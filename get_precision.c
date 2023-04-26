08d01ec1e08d8e26bc7d0d53103fe1833efecce9
charteym <charteym@gmail.com>
2023-04-25 20:17:25 -0700

    C - printf

--- get_precision.c
+++ get_precision.c
@@ -3,7 +3,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
  * get_precision - Calculates the precision for printing
- * By charteym and levin Lavatsa
+ * By charteym & levin Lavatsa
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
